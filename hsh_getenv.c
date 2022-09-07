9b30b8e02ae7f70c24b26f66ddb7d99d1fb31330
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-09-06 20:02:09 -0700

    all tasks

---
+++ hsh_getenv.c
@@ -0,0 +1,23 @@
+es (17 sloc)  263 Bytes
+
+#include "shell.h"
+
+
+/**
+ * _getenv - prts env to standard output
+ *
+ * @env: enviroment
+ *
+ */
+
+void _getenv(char **env)
+{
+	size_t run = 0;
+
+	while (env[run])
+	{
+		write(STDOUT_FILENO, env[run], _strlen(env[run]));
+		write(STDOUT_FILENO, "\n", 1);
+		run++;
+	}
+}
